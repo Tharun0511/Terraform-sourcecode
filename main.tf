248f408a9762b54044df0af0b87c5174c60386ce
Tharun <145205951+Tharun0511@users.noreply.github.com>
2025-01-07 12:06:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  client_id       = "ef3e16e1-6387-4710-ab15-cf60848e70cf"
-  client_secret   = "yvK8Q~oYSbwBTg~phU2CjLUSoAIRMlnMG7GgGcUc"
-  tenant_id       = "d614d2c6-b0b3-4a53-bb50-847e747c88d7"
-  subscription_id = "9dfcfea7-c731-4d13-91e2-b9aefaff8bf1"
+ # client_id       = "ef3e16e1-6387-4710-ab15-cf60848e70cf"
+ # client_secret   = "yvK8Q~oYSbwBTg~phU2CjLUSoAIRMlnMG7GgGcUc"
+ # tenant_id       = "d614d2c6-b0b3-4a53-bb50-847e747c88d7"
+ # subscription_id = "9dfcfea7-c731-4d13-91e2-b9aefaff8bf1"
 }
